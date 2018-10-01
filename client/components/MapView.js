62b4b81a300f6e70325f9d714a2ab4bd6741dc0a
Alex Morson <amorson@conncoll.edu>
2018-10-01 09:21:55 -0500

    working on MapView, basic structure conceived

--- client/components/MapView.js
+++ client/components/MapView.js
@@ -1,1 +1,31 @@
 import React, {Component} from 'react';
+import { GoogleApiWrapper, Map, Marker } from 'google-maps-react';
+
+import Typogragphy from '@material-ui/core/Typography';
+import { typgoraphy } from '@material-ui/core/styles';
+
+class MapView extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            infoWindowDisplayed: false,
+            selectedMarker: {}
+        }
+    }
+
+    //For each campsite in campsites, render a marker. If clicked on, it should open the modal page. If hovered over, it should display a small info window?
+    render() {
+        const { campsites, campsite } = this.props;
+        return (
+            <Map google={this.props.google} initialCenter={{lat: 37.856, lng: -16.89034}} className={'map'} zoom={16}>
+                {campsites.map(camp => {
+                    return <Marker key={camp.id} name={camp.name} position={camp.location} />
+                })}
+            </Map>
+        );
+    }
+}
+
+export default GoogleApiWrapper({
+    api: (process.env.GOOGLE_MAPS_API_KEY)
+})(MapView);
