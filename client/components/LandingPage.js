0da1afec46a1535c28756a527bcddabe44453622
Alex Morson <amorson@conncoll.edu>
2018-10-02 19:49:57 -0500

    added filteredCampsites substore and have the filters ready for testing

--- client/components/LandingPage.js
+++ client/components/LandingPage.js
@@ -2,17 +2,20 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import CampsiteCollection from './CampsiteCollection';
 import MapView from './MapView';
+import Amenities from './Amenities';
 import { fetchCampsites } from '../store/campsites';
 import { fetchAmenities } from '../store/amenities';
 import { fetchReservations } from '../store/reservations';
 import { fetchCampsite } from '../store/campsite';
+import { getFilteredCampsites } from '../store/filteredCampsites';
 
 const mapStateToProps = state => {
   return {
     campsites: state.campsites,
     campsite: state.campsite,
     amenities: state.amenities,
-    reservations: state.reservations
+    reservations: state.reservations,
+    filteredCampsites: state.filteredCampsites
   }
 }
 
@@ -20,41 +23,91 @@ const mapDispatchToProps = dispatch => ({
   fetchCampsites: () => dispatch(fetchCampsites()),
   fetchAmenities: () => dispatch(fetchAmenities()),
   fetchCampsite: id => dispatch(fetchCampsite(id)),
-  fetchReservations: () => dispatch(fetchReservations())
+  fetchReservations: () => dispatch(fetchReservations()),
+  getFilteredCampsites: (campsites, selectedAmenities, timeframe, typing) => dispatch(getFilteredCampsites(campsites, selectedAmenities, timeframe, typing))
 })
 
 class LandingPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      campsites: [],
-      campsite: {},
-      amenities: [],
-      reservations: []
+      selectedAmenities: [],
+      typing: '',
+      startTime: null,
+      endTime: null,
+      reservations: [],
     }
+    this.onAmenitiesChange = this.onAmenitiesChange.bind(this);
+    this.onStartTimeChange = this.onStartTimeChange.bind(this);
+    this.onEndTimeChange = this.onEndTimeChange.bind(this);
+    this.onTypingChange = this.onTypingChange.bind(this);
   }
 
   async componentDidMount(){
     const campsites = await this.props.fetchCampsites();
     // const amenities = await this.props.fetchAmenities();
     const reservations = await this.props.fetchReservations();
+    this.props.getFilteredCampsites(campsites, [], null, null, '');
     this.setState({
-      campsites: campsites,
       // amenities: amenities,
       reservations: reservations
     });
   }
 
+  onAmenitiesChange(e) {
+    const selectedAmenities = this.state.selectedAmenities;
+    if (e.target.checked) {
+      selectedAmenities.push(e.target.value);
+    }
+    else {
+      let index = selectedAmenities.indexOf(e.target.value);
+      selectedAmenities.splice(index, 1);
+    }
+    this.setState({ selectedAmenities: selectedAmenities });
+    this.props.getFilteredCampsites(this.props.campsites, this.state.selectedAmenities, this.state.startTime, this.state.endTime, this.state.typing);
+  }
+
+  onStartTimeChange(e) {
+    const startTime = e.target.value;
+    const endTime = this.state.endTime;
+    this.setState({
+      startTime: startTime
+    });
+    if (endTime) {
+      this.props.getFilteredCampsites(this.props.campsites, this.state.selectedAmenities, this.state.startTime, endTime, this.state.typing);
+    }
+  }
+
+  onEndTimeChange(e) {
+    const endTime = e.target.value;
+    const startTime = this.state.startTime;
+    this.setState({
+      endTime: endTime
+    });
+    if (startTime) {
+      this.props.getFilteredCampsites(this.props.campsites, this.state.selectedAmenities, startTime, this.state.endTime, this.state.typing);
+    }
+  }
+
+  onTypingChange(e) {
+    const typing = e.target.value;
+    this.setState({
+      typing: typing
+    });
+    this.props.getFilteredCampsites(this.props.campsites, this.state.selectedAmenities, this.state.startTime, this.state.endTime, typing);
+  }
+
   render() {
-    const {campsites, amenities, campsite, reservations } = this.props;
+    const {campsites, amenities, campsite, reservations, filteredCampsites } = this.props;
     return (
       <div className="MainContainer">
         <div className="ParallaxContainer">{/* <h1>Aloha!</h1> */}</div>
 
         <div className="ContentContainer">
           <div className="Content">
-            <MapView campsites={campsites} campsite={campsite} />
-            <CampsiteCollection campsites={campsites} campsite={campsite} />
+            <Amenities />
+            <MapView campsites={filteredCampsites} campsite={campsite} />
+            <CampsiteCollection campsites={filteredCampsites} campsite={campsite} />
             {/* REMOVE <p> TAGS AND PUT YOUR COMPONENTS INSIDE OF CONTENT CONTAINER */}
             
           </div>
---
+++ client/store/filteredCampsites.js
@@ -0,0 +1,34 @@
+//ACTION TYPES
+const GET_FILTERED_CAMPSITES = 'GET_FILTERED_CAMPSITES';
+
+//ACTION CREATORS
+const getFilteredCamps = campsites => ({
+    type: GET_FILTERED_CAMPSITES,
+    campsites
+});
+
+//REDUCER
+const reducer = (filteredCampsites = [], action) => {
+    switch (action.type) {
+        case GET_FILTERED_CAMPSITES:            
+            return action.campsites;
+        default:
+            return filteredCampsites;
+    }
+}
+
+//THUNK MIDDLEWARE
+export function getFilteredCampsites(campsites, selectedAmenities, startTime, endTime, typing) {
+    return dispatch => {
+        const filteredCamps = campsites.filter(camp => {
+            return (
+                (!selectedAmenities.length || selectedAmenities.every(amenity => camp.amenities.includes(amenity))) &&
+                ((!startTime || !endTime) || camp.reservations.filter(reservation => !reservation.daysBooked().includes(startTime) && !reservation.daysBooked().includes(endTime))) &&
+                (!typing || typing === camp.type)
+            );
+        });
+        dispatch(getFilteredCamps(filteredCamps));
+    }
+}
+
+export default reducer;
