52bcadfb7625fd26361e6eb1c900d9162dd6bda2
Shane EC Davis <secd0183@yahoo.com>
2018-10-03 15:25:49 -0500

    added admin dashboard home component, also added 2 links to navbar

---
+++ client/components/AdminDashboard.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+
+class AdminDashboard extends Component {
+
+  render(){
+    return (<div> dashboard</div>)
+  }
+}
+
+export default AdminDashboard
