92f44e5fb15a3a041af26239e3702eb9ab202c82
Shane EC Davis <secd0183@yahoo.com>
2018-10-03 13:55:00 -0500

    basic icon showing on map and map aligned

--- client/components/MapView.js
+++ client/components/MapView.js
@@ -1,36 +1,51 @@
-import React, {Component} from 'react';
-import { GoogleApiWrapper, Map, Marker } from 'google-maps-react';
-import { GOOGLE_MAPS_API_KEY } from '../secrets';
+import React, {Component} from 'react'
+import {GoogleApiWrapper, Map, Marker} from 'google-maps-react'
+import {GOOGLE_MAPS_API_KEY} from '../secrets'
 
-import Typogragphy from '@material-ui/core/Typography';
-import { typgoraphy } from '@material-ui/core/styles';
+import Typogragphy from '@material-ui/core/Typography'
+import {typgoraphy} from '@material-ui/core/styles'
 // if (process.env.NODE_ENV !== 'production') require('../secrets')
 
-
 class MapView extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            infoWindowDisplayed: false,
-            selectedMarker: {}
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      infoWindowDisplayed: false,
+      selectedMarker: {}
     }
+  }
 
-    //For each campsite in campsites, render a marker. If clicked on, it should open the modal page. If hovered over, it should display a small info window?
-    render() {
-        const { campsites, campsite } = this.props;
-        return (
-            <Map google={this.props.google} initialCenter={{lat: 42.856, lng: -86.89034}} zoom={9}>
-                {campsites.map(camp => {
-                    return <Marker key={camp.id} name={camp.name} position={camp.location} />
-                })}
-            </Map>
-        );
-    }
+  //For each campsite in campsites, render a marker. If clicked on, it should open the modal page. If hovered over, it should display a small info window?
+  render() {
+    const {campsites, campsite} = this.props
+    return (
+      <Map
+        google={this.props.google}
+        initialCenter={{lat: 43.769737, lng: -89.202131}}
+        zoom={17}
+      >
+        {campsites.map(camp => {
+          return (
+            <Marker
+              key={camp.id}
+              name={camp.name}
+              position={{
+                lat: camp.location.coordinates[0],
+                lng: camp.location.coordinates[1]
+              }}
+              icon={{
+                url: '/campericon.png'
+              }}
+            />
+          )
+        })}
+      </Map>
+    )
+  }
 }
 
 const wrapperContainer = GoogleApiWrapper({
-    apiKey: GOOGLE_MAPS_API_KEY
-})(MapView);
+  apiKey: GOOGLE_MAPS_API_KEY
+})(MapView)
 
-export default wrapperContainer;
+export default wrapperContainer
