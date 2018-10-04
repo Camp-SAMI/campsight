f807ec08fcb34a9e3273685e242e6ae1c7c9493d
Ezugworie I.e <ezugworie.ikechukwu@live.com>
2018-10-04 15:49:50 -0500

    fixed jsx on map view and bounded checks on campsite detail

--- client/components/CampsiteDetail.js
+++ client/components/CampsiteDetail.js
@@ -33,130 +33,137 @@ class CampsiteDetail extends Component {
       amenities,
       cost
     } = this.props.campsite
-    return (
-      <Fragment>
-        <Container>
-          <Divider hidden />
-          <Segment clearing>
-            <Header as="h1" floated="left">
-              {`Welcome to CAMPSIGHT-${name}`}
-            </Header>
-            <Header as="h2" floated="right">{`${formatPrice(
-              cost
-            )}/DAY`}</Header>
-          </Segment>
-          <Divider hidden />
-          <Grid stackable columns={2}>
-            <Grid.Column>
-              <Fragment>
-                <Image size="huge" src={coverImage} />
-                <Divider hidden />
-                <Header as="h2">
-                  Description
-                  <Header.Subheader>{desc}</Header.Subheader>
-                </Header>
-                <Divider hidden />
-                <Header as="h2">Amenities on Site</Header>
-                <List divided relaxed>
-                  {amenities && amenities.length ? (
-                    amenities.map(amenity => (
-                      <List.Item key={amenity.category}>
-                        <List.Icon
-                          name={amenity.category || 'bandcamp'}
-                          size="large"
-                          verticalAlign="middle"
-                        />
-                        <List.Content>
-                          <List.Header as="h4">{amenity.category}</List.Header>
-                        </List.Content>
-                      </List.Item>
-                    ))
-                  ) : (
-                    <Fragment>
-                      <List.Item>
-                        <List.Icon
-                          name="bathtub"
-                          size="large"
-                          verticalAlign="middle"
-                        />
-                        <List.Content>
-                          <List.Header as="h4">Bathroom</List.Header>
-                        </List.Content>
-                      </List.Item>
-                      <List.Item>
-                        <List.Icon
-                          name="wifi"
-                          size="large"
-                          verticalAlign="middle"
-                        />
-                        <List.Content>
-                          <List.Header as="h4">Wi-Fi</List.Header>
-                        </List.Content>
-                      </List.Item>
-                      <List.Item>
-                        <List.Icon
-                          name="power cord"
-                          size="large"
-                          verticalAlign="middle"
-                        />
-                        <List.Content>
-                          <List.Header as="h4">Electricity</List.Header>
-                        </List.Content>
-                      </List.Item>
-                      <List.Item>
-                        <List.Icon
-                          name="cuttlefish"
-                          size="large"
-                          verticalAlign="middle"
-                        />
-                        <List.Content>
-                          <List.Header as="h4">Fishing</List.Header>
-                        </List.Content>
-                      </List.Item>
-                    </Fragment>
-                  )}
-                </List>
-              </Fragment>
-            </Grid.Column>
-            <Grid.Column>
-              <Fragment>
-                <Image.Group size="small">
-                  {images && images.length ? (
-                    images.map(image => <Image key={image} src={image} />)
-                  ) : (
-                    <Fragment>
-                      <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
-                      <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
-                      <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
-                      <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
-                      <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
-                      <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
-                      <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
-                      <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
-                      <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
-                      {/* <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
+
+    if (name && coverImage && images && desc && amenities && cost) {
+      return (
+        <Fragment>
+          <Container>
+            <Divider hidden />
+            <Segment clearing>
+              <Header as="h1" floated="left">
+                {`Welcome to CAMPSIGHT-${name && name}`}
+              </Header>
+              <Header as="h2" floated="right">{`${formatPrice(
+                cost && cost
+              )}/DAY`}</Header>
+            </Segment>
+            <Divider hidden />
+            <Grid stackable columns={2}>
+              <Grid.Column>
+                <Fragment>
+                  <Image size="huge" src={coverImage} />
+                  <Divider hidden />
+                  <Header as="h2">
+                    Description
+                    <Header.Subheader>{desc}</Header.Subheader>
+                  </Header>
+                  <Divider hidden />
+                  <Header as="h2">Amenities on Site</Header>
+                  <List divided relaxed>
+                    {amenities && amenities.length ? (
+                      amenities.map(amenity => (
+                        <List.Item key={amenity.category}>
+                          <List.Icon
+                            name={amenity.category || 'bandcamp'}
+                            size="large"
+                            verticalAlign="middle"
+                          />
+                          <List.Content>
+                            <List.Header as="h4">
+                              {amenity.category}
+                            </List.Header>
+                          </List.Content>
+                        </List.Item>
+                      ))
+                    ) : (
+                      <Fragment>
+                        <List.Item>
+                          <List.Icon
+                            name="bathtub"
+                            size="large"
+                            verticalAlign="middle"
+                          />
+                          <List.Content>
+                            <List.Header as="h4">Bathroom</List.Header>
+                          </List.Content>
+                        </List.Item>
+                        <List.Item>
+                          <List.Icon
+                            name="wifi"
+                            size="large"
+                            verticalAlign="middle"
+                          />
+                          <List.Content>
+                            <List.Header as="h4">Wi-Fi</List.Header>
+                          </List.Content>
+                        </List.Item>
+                        <List.Item>
+                          <List.Icon
+                            name="power cord"
+                            size="large"
+                            verticalAlign="middle"
+                          />
+                          <List.Content>
+                            <List.Header as="h4">Electricity</List.Header>
+                          </List.Content>
+                        </List.Item>
+                        <List.Item>
+                          <List.Icon
+                            name="cuttlefish"
+                            size="large"
+                            verticalAlign="middle"
+                          />
+                          <List.Content>
+                            <List.Header as="h4">Fishing</List.Header>
+                          </List.Content>
+                        </List.Item>
+                      </Fragment>
+                    )}
+                  </List>
+                </Fragment>
+              </Grid.Column>
+              <Grid.Column>
+                <Fragment>
+                  <Image.Group size="small">
+                    {images && images.length ? (
+                      images.map(image => <Image key={image} src={image} />)
+                    ) : (
+                      <Fragment>
+                        <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
+                        <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
+                        <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
+                        <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
+                        <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
+                        <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
+                        <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
+                        <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
+                        {/* <Image src="https://react.semantic-ui.com/images/wireframe/image.png" /> */}
+                        {/* <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
                       <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
                       <Image src="https://react.semantic-ui.com/images/wireframe/image.png" /> */}
-                    </Fragment>
-                  )}
-                </Image.Group>
-                <Divider hidden />
-                <Fragment>
-                  <Header as="h2">Reserve Campsite</Header>
-                  <ReservationForm
-                    id={Number(this.props.id || this.props.match.params.id)}
-                    // fetchLatestCampsiteReservation={
-                    //   fetchLatestCampsiteReservation
-                    // }
-                  />
+                      </Fragment>
+                    )}
+                  </Image.Group>
+                  <Divider hidden />
+                  <Fragment>
+                    <Header as="h2">Reserve Campsite</Header>
+                    <ReservationForm
+                      id={Number(this.props.id || this.props.match.params.id)}
+                      // fetchLatestCampsiteReservation={
+                      //   fetchLatestCampsiteReservation
+                      // }
+                    />
+                  </Fragment>
                 </Fragment>
-              </Fragment>
-            </Grid.Column>
-          </Grid>
-          <Divider hidden />
-        </Container>
-      </Fragment>
-    )
+              </Grid.Column>
+            </Grid>
+            <Divider hidden />
+          </Container>
+        </Fragment>
+      )
+    } else {
+      return null
+    }
   }
 }
 
