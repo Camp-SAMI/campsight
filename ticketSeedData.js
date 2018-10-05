473292ff551fee60d749a523fd99824a1c56e2c5
Alex Morson <amorson@conncoll.edu>
2018-10-05 17:39:56 -0500

    data to make tickets for the seed is made.

--- ticketSeedData.js
+++ ticketSeedData.js
@@ -25,14 +25,14 @@ const ticketData = [
         email: 'ulyssesshot@hotmail.com',
         content: 'At night, we keep getting racoons coming near our tent, even though we dispose of our garbage properly. They make a lot of noise, which makes it hard to sleep.',
         priority: '2',
-        status: 'closed'
+        status: 'close'
     },
     {
         title: 'Neighbor is too noisy',
         email: 'gazelle980@hotmail.com',
         content: 'The person staying in the tent next to my campsite makes way too much noise at night; he constantly shouts profanity at ungodly hours. I already tried talking to him myself and nothing changed, so it would be nice if someone else could get him to stop.',
         priority: '2',
-        status: 'closed'
+        status: 'close'
     },
     {
         title: 'Not sure where to get firewood',
