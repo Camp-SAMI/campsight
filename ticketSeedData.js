300ff836a631db8f10b446d21330593a4561c02c
Alex Morson <amorson@conncoll.edu>
2018-10-05 17:16:14 -0500

    data to make tickets for the seed is made.

---
+++ ticketSeedData.js
@@ -0,0 +1,46 @@
+const ticketData = [
+    {
+        title: 'Toilet Broken',
+        email: 'cody1@email.com',
+        content: "The toilet close to Heron appears to have broken down; it won't flush at all.",
+        priority: '3',
+        status: 'in-progress'
+    },
+    {
+        title: 'Tree by 154 fell down',
+        email: 'isisheen89@gmail.com',
+        content: "One of the trees close to our campsite has fallen over after a thunderstorm. It's taking up a lot of space, so it would be great if you could clear it out.",
+        priority: '5',
+        status: 'open'
+    },
+    {
+        title: 'Bed in Badger Cabin is broken',
+        email: 'bagginsy7ik@yahoo.com',
+        content: 'The bed by the eastern wall in Badger Cabin collapsed in the middle, so the matress is sagging down to the floor.',
+        priority: '4',
+        status: 'open'
+    },
+    {
+        title: 'Too many racoons nearby',
+        email: 'ulyssesshot@hotmail.com',
+        content: 'At night, we keep getting racoons coming near our tent, even though we dispose of our garbage properly. They make a lot of noise, which makes it hard to sleep.',
+        priority: '2',
+        status: 'closed'
+    },
+    {
+        title: 'Neighbor is too noisy',
+        email: 'gazelle980@hotmail.com',
+        content: 'The person staying in the tent next to my campsite makes way too much noise at night; he constantly shouts profanity at ungodly hours. I already tried talking to him myself and nothing changed, so it would be nice if someone else could get him to stop.',
+        priority: '2',
+        status: 'closed'
+    },
+    {
+        title: 'Not sure where to get firewood',
+        email: 'kferguson@zippo.net',
+        content: 'Could someone direct me on where I can find firewood prechopped? Thank you.',
+        priority: 'null',
+        status: 'open'
+    }
+];
+
+module.exports = ticketData;
