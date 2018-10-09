4113b9a7bea5a1be3922e8807c33fd3c24d19622
pindimindi <milicarodic307@yahoo.com>
2018-10-09 11:17:24 -0500

    updated campsite cards

--- client/components/CampsiteBlock.js
+++ client/components/CampsiteBlock.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react'
 import {NavLink} from 'react-router-dom'
 import {connect} from 'react-redux'
-import {Card, Image} from 'semantic-ui-react'
+import {Card, Image, Icon} from 'semantic-ui-react'
+import {formatPrice} from '../utils/formatPrice'
 
 const CampsiteBlock = props => {
   const {campsite} = props
@@ -10,7 +11,14 @@ const CampsiteBlock = props => {
       <Card>
         <Image src={campsite.coverImage} />
         <Card.Content>
-          <Card.Header> {campsite.name}</Card.Header>
+          <Card.Header>Name: {campsite.name}</Card.Header>
+          <Card.Meta>
+            <span>Type: {campsite.typing}</span>
+          </Card.Meta>
+          <Card.Content extra>
+            <Icon name="money bill alternate outline" />{' '}
+            {formatPrice(campsite.cost)} /Day
+          </Card.Content>
           {/* <Card.Description>{campsite.desc}</Card.Description> */}
         </Card.Content>
       </Card>
