f42920f1b72eae661de89011cbdc0fd1d67669f7
pindimindi <milicarodic307@yahoo.com>
2018-10-10 09:58:39 -0500

    toggle needs fix

--- client/store/analiticsToggle.js
+++ client/store/analiticsToggle.js
@@ -2,7 +2,7 @@ const TOGGLE_ANALITICS = 'TOGGLE_ANALITICS'
 
 export const toggleAnalitics = () => ({type: TOGGLE_ANALITICS})
 
-export default function reducer(toggle = false, action) {
+export default function reducer(toggle = true, action) {
   switch (action.type) {
     case TOGGLE_ANALITICS:
       return !toggle
